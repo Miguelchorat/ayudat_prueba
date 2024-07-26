4d8b96a625075dd927d086c35d95f3e71391bee0
Miguel Angel Chorat Delgado <miguelchorat@gmail.com>
2024-07-26 11:36:47 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <JoinSection />
+  </div>
+</template>
+<script>
+
+import JoinSection from '@/components/JoinSection.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    JoinSection
+  }
+}
+</script>
